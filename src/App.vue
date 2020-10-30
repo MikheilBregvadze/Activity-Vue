cb1096365f8992b31fef346e665d8f6b80d614c4
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2020-10-30 18:53:53 +0400

    add vue componeents

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,200 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="activityApp">
+    <nav class="navbar is-white topNav">
+      <div class="container">
+        <div class="navbar-brand">
+          <h1>{{ fullAppName }}</h1>
+          <!-- <h1>{{ watchedAppName }}</h1> -->
+        </div>
+      </div>
+    </nav>
+    <the-navbar />
+    <section class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <activity-create :categories="categories" @activityCreated="addActivity" />
+        </div>
+        <div class="column is-9">
+          <div class="box content" :class="{fetching: isFetching, 'has-error': error}">
+            <div v-if="error">
+              {{ error }}
+            </div>
+            <div v-else>
+              <div v-if="isFetching">
+                Loading...
+              </div>
+              <activity-item v-for="activity in activities" 
+                             :key="activity.id"
+                             :activity="activity" 
+              />
+            </div>
+            <div v-if="!isFetching && !error">
+              <div class="activity-length">
+                Currently {{ ativityLength }} activity
+              </div>
+              <div class="activity-motivation">
+                {{ activityMotivation }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+
+import ActivityItem from './components/ActivityItem'
+import ActivityCreate from './components/ActivityCreate'
+import TheNavbar from './components/TheNavbar'
+import { fetchActivities, fetchCategories, fetchUser } from './api/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ActivityItem,
+    ActivityCreate,
+    TheNavbar
+  },
+  data() {
+    return {
+      creator: 'Mikheil Bregvadze',
+      appName: 'Activity planner',
+      watchedAppName: 'Activity app by Gela Gnolidze',
+      isFetching: false,
+      error: null,
+      user: {},
+      activities: {},
+      categories: {}
+    }
+  },
+
+  computed: {
+    fullAppName() {
+      return this.appName + ' by ' + this.creator;
+    },
+
+    ativityLength() {
+      return Object.keys(this.activities).length;
+    },
+
+    activityMotivation() {
+      if(this.ativityLength && this.ativityLength < 5) {
+        return 'Nice to see some activities (:'
+      } else if(this.ativityLength >= 5) {
+        return 'So many activities! Good job!'
+      } else {
+        return 'No activities, so sad'
+      }
+    }
+  },
+
+  created() {
+    this.isFetching = true;
+    this.activities = fetchActivities()
+      .then(activities => {
+        this.isFetching = false;
+        this.activities = activities;
+      })
+      .catch(err => {
+        this.error = err;
+        this.isFetching = false;
+      })
+    this.user = fetchUser();
+    this.categories = fetchCategories();
+  },
+
+  methods: {
+    addActivity(newActivity) {
+      Vue.set(this.activities, newActivity.id, newActivity);
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#activityApp {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+  html,body {
+    font-family: 'Open Sans', serif;
+    background: #F2F6FA;
+  }
+  footer {
+    background-color: #F2F6FA !important;
+  }
+  .fetching {
+    border: 2px solid orange;
+  }
+  .has-error {
+    border: 2px solid red;
+  }
+  .activity-motivation {
+    float: right;
+  }
+  .activity-length {
+    display: inline-block;
+  }
+  .topNav {
+    border-top: 5px solid #3498DB;
+  }
+  .topNav .container {
+    border-bottom: 1px solid #E6EAEE;
+  }
+  .container .columns {
+    margin: 3rem 0;
+  }
+  .navbar-menu .navbar-item {
+    padding: 0 2rem;
+  }
+  aside.menu {
+    padding-top: 3rem;
+  }
+  aside.menu .menu-list {
+    line-height: 1.5;
+  }
+  aside.menu .menu-label {
+    padding-left: 10px;
+    font-weight: 700;
+  }
+  .button.is-primary.is-alt {
+    background: #00c6ff;
+    background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
+    background: linear-gradient(to bottom, #0072ff, #00c6ff);
+    font-weight: 700;
+    font-size: 14px;
+    height: 3rem;
+    line-height: 2.8;
+  }
+  .media-left img {
+    border-radius: 50%;
+  }
+  .media-content p {
+    font-size: 14px;
+    line-height: 2.3;
+    font-weight: 700;
+    color: #8F99A3;
+  }
+  article.post {
+    margin: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #E6EAEE;
+  }
+  article.post:last-child {
+    padding-bottom: 0;
+    border-bottom: none;
+  }
+  .menu-list li{
+    padding: 5px;
+  }
+  
+  .navbar-brand > h1 {
+    font-size: 31px;
+    padding: 20px;
+  }
 </style>
---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <article class="post">
+    <h4>{{ activity.title }}</h4>
+    <div class="media">
+      <div class="media-left">
+        <p class="image is-32x32">
+          <img src="../assets/user.png">
+        </p>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <a href="#">Filip Jerga</a> updated {{ activity.updatedAt }} minutes ago &nbsp;
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span>Progress: <span :class="`color-${activityProgress}`">{{ activity.progress }} %</span></span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    activity: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    activityProgress() {
+      const progress = this.activity.progress;
+
+      if(progress <= 0) {
+        return 'red'
+      } else if(progress <= 50) {
+        return 'orange'
+      } else {
+        return 'green'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .color-red {
+    color: red;
+  }
+
+  .color-orange {
+    color: orange;
+  }
+
+  .color-green {
+    color: green;
+  }
+</style>
